c1517dd129f612eb511fa4a79fe81540696732dc
harsh9975 <33349461+harsh9975@users.noreply.github.com>
2019-09-20 05:39:27 +0530

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCqfDgRq8UAjRcFNN4OU7_X6YjcZHq4OXQ",
+    authDomain: "ambulance-cb0df.firebaseapp.com",
+    databaseURL: "https://ambulance-cb0df.firebaseio.com",
+    projectId: "ambulance-cb0df",
+    storageBucket: "",
+    messagingSenderId: "140472689264",
+    appId: "1:140472689264:web:5e85c9770e86f8981f0ab5"
+});
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  verificationId: any;
+  code: string = "";
+  phone: string = "";
+
   constructor() {}
+
+  send(){
+    (<any>window).FirebasePlugin.verifyPhoneNumber(9975482603,60, (credential) =>{
+      alert("SMS sent successfully");
+      console.log(credential);
+
+      this.verificationId = credential.verificationId;
+    }, function(error){
+      console.error(error);
+    });
+  }
+
+  verify(){
+    let signinCredential = firebase.auth.PhoneAuthProvider.credential(this.verificationId, this.code);
+    firebase.auth().signInWithCredential(signinCredential).then((info)=>{
+      console.log(info);
+    }, (error)=>{
+      console.log(error)
+    });
+  }
 
 }
